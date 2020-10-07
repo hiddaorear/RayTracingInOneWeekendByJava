bdc4be9b8fa1965b84af50ec719612b54bd95d04
siegelwang <siegelwang@tencent.com>
2020-10-07 22:21:53 +0800

    完成第七章

---
+++ chapter-07/src/raytracing/Camera.java
@@ -0,0 +1,24 @@
+package raytracing;
+
+public class Camera {
+    private Vec3 lower_left; // 画布左下角点
+    private Vec3 horizontal; // 宽
+    private Vec3 vertical; // 高
+    private Vec3 origin; // 相机原点
+
+    public Camera() {
+        lower_left = new Vec3(-2.0f, -1.0f, -1.0f);
+        horizontal = new Vec3(4.0f, 0.0f, 0.0f);
+        vertical = new Vec3(0.0f, 2.0f, 0.0f);
+        origin = new Vec3(0.0f, 0.0f, 0.0f);
+    }
+
+    /**
+     * @param u 距离lower_left的横向距离
+     * @param v 距离lower_left的纵向距离
+     * @return 光线向量
+     */
+    public Ray GetRay(float u, float v) {
+        return new Ray(origin, lower_left.Add(horizontal.Scale(u)).Add(vertical.Scale(v)));
+    }
+}
---
+++ chapter-07/src/raytracing/HitRecord.java
@@ -0,0 +1,13 @@
+package raytracing;
+
+public class HitRecord {
+    public float t; // 相撞的时间
+    public Vec3 p; // 相撞的坐标
+    public Vec3 normal; // 相撞点的法向量
+
+    public HitRecord() {
+        t = 0;
+        p = new Vec3(0, 0, 0);
+        normal = new Vec3(0, 0, 0);
+    }
+}
